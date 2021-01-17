ac7c5e3bafa83bdbc8870157d6d63813c637cd6e
Gonzalo Gorgojo <gongorgojo@gmail.com>
2021-01-17 03:45:09 -0300

    Todo el Proyecto
    -Estructura
    -Diseño
    -Logica

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# casa de cambio
- Interfaz para mostrar los tipos de cambios utilizando API y testando con cypress
+# Casa De Cambio
+ 
+ 
+> Proyecto para mostrar los tipos de cambio,<br/>
+> pudiendo seleccionar moneda de base y fechas.
+
+Fue creando utilizando Vanilla Js, Bootstrap, APIs y Testeada con Cypress.
---
+++ src/index.js
@@ -0,0 +1,75 @@
+let $fecha = document.querySelector('#fecha');
+let $monedas = document.querySelector('#moneda');
+let $botonBusqueda = document.querySelector('#botonBusqueda');
+
+
+function mostrarFechaActual() {
+    let hoy = (new Date()).toISOString().split('T')[0];
+    $fecha.value = hoy;
+    $fecha.setAttribute('max', hoy);
+}
+
+function obtenerDatos() {
+    return fetch('https://api.exchangeratesapi.io/latest?base=USD')
+        .then((respuesta) => respuesta.json())
+        .then((respuesta) => respuesta.rates)
+        .then((monedas) => Object.keys(monedas).concat('EUR'))
+        .then((monedas) => mostrarMonedas(monedas))
+}
+
+function mostrarMonedas(monedas) {
+    monedas.forEach(base => {
+        const $item = document.createElement('option');
+        $item.value = base;
+        $item.textContent = base;
+
+        $item.addEventListener('click', () => {
+        });
+        $monedas.appendChild($item);
+    });
+
+}
+
+obtenerDatos();
+mostrarFechaActual();
+
+$botonBusqueda.onclick = function () {
+    let fechaSeleccionada = $fecha.value
+    let monedaSeleccionada = $monedas.value
+
+    pedirCambios(monedaSeleccionada, fechaSeleccionada)
+}
+
+
+
+function pedirCambios(monedaSeleccionada, fechaSeleccionada) {
+
+    return fetch(`https://api.exchangeratesapi.io/${fechaSeleccionada}?base=${monedaSeleccionada}`)
+        .then(response => response.json())
+        .then((respuesta) => respuesta.rates)
+        .then((valor) => {
+            crearTabla(valor);
+        });
+}
+
+function crearTabla(valor) {
+    const $tabla = document.querySelector('#tabla tbody');
+    $tabla.innerHTML = '';
+    Object.keys(valor).sort().forEach((moneda, i) => {
+        const $fila = document.createElement('tr');
+        const $indice = document.createElement('td');
+        const $moneda = document.createElement('td');
+        const $cambio = document.createElement('td');
+        const $br = document.createElement('br');
+        $indice.textContent = i + 1;
+        $moneda.textContent = moneda;
+        $cambio.textContent = valor[moneda];
+        $fila.appendChild($indice);
+        $fila.appendChild($moneda);
+        $fila.appendChild($cambio);
+        $fila.appendChild($br);
+        $tabla.appendChild($fila);
+
+    })
+}
+
